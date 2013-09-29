c4b30cff952be4da2e2babc874b4c3d2dff81a98
Jakub Wiśniowski <restless.being@gmail.com>
2013-09-29 16:12:26 +0200

    added line 2 as address complement

--- src/postal/forms/fr/forms.py
+++ src/postal/forms/fr/forms.py
@@ -7,11 +7,11 @@ from postal.forms import PostalAddressForm
 
 class FRPostalAddressForm(PostalAddressForm):
     line1 = forms.CharField(label=_(u"Street"), max_length=100)
+    line2 = forms.CharField(label=_(u"Address Complement"), max_length=100)
     city = forms.CharField(label=_(u"City"), max_length=100)
     code = FRZipCodeField(label=_(u"Zip code"))
 
     def __init__(self, *args, **kwargs):
         super(FRPostalAddressForm, self).__init__(*args, **kwargs)
-        self.fields.pop('line2')
         self.fields.pop('state')
         self.fields['country'].initial = "FR"
